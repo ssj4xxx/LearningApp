1146b92800d1435ff72136d7863863cec797ec6d
ChaRedmiBook <varus552@gmail.com>
2022-07-27 01:08:18 +0700

    9.7 Account Completed

--- src/com/khoalt/IntroductionChap9/Ex97/AccountManager.java
+++ src/com/khoalt/IntroductionChap9/Ex97/AccountManager.java
@@ -1,4 +1,20 @@
 package com.khoalt.IntroductionChap9.Ex97;
 
+import java.util.Scanner;
+
 public class AccountManager {
+    static Account firstAccount = new Account(1122, 20000, 0.045);
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Account " + firstAccount.getId() + ": Balance - " + firstAccount.getBalance() + "$, Monthy Interest - " + firstAccount.getMonthlyInterest() + "$");
+        input.nextLine();
+        System.out.println("Withdraw 2500");
+        firstAccount.withdraw(2500);
+        System.out.print("Account " + firstAccount.getId() + ": Balance - " + firstAccount.getBalance() + "$, Monthy Interest - " + firstAccount.getMonthlyInterest() + "$");
+        input.nextLine();
+        System.out.println("Deposit 3000");
+        firstAccount.deposit(3000);
+        System.out.println("Account " + firstAccount.getId() + ": Balance - " + firstAccount.getBalance() + "$, Monthy Interest - " + firstAccount.getMonthlyInterest() + "$");
+    }
 }
