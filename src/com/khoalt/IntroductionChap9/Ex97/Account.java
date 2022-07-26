82c221db7e38ba5d7e11283b6bc8e1bc10cc0bcd
ChaRedmiBook <varus552@gmail.com>
2022-07-26 18:32:33 +0700

    StopWatch complete 2

--- src/com/khoalt/IntroductionChap9/Ex97/Account.java
+++ src/com/khoalt/IntroductionChap9/Ex97/Account.java
@@ -11,7 +11,45 @@ public class Account {
     public Account() {
     }
 
-    public Account(int id, double balance, double annualInterestRate, Date dateCreated) {
+    public Account(int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+    }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return annualInterestRate / 12;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate();
+    }
+
+    public boolean withdraw(int amount) {
+        this.balance = balance - amount;
+        return true;
+    }
+    public boolean deposit(int amount) {
+        this.balance = balance + amount;
+        return true;
     }
 }
---
+++ src/com/khoalt/IntroductionChap9/Ex97/AccountManager.java
@@ -0,0 +1,4 @@
+package com.khoalt.IntroductionChap9.Ex97;
+
+public class AccountManager {
+}
