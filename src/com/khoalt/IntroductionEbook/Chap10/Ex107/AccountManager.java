16c62faea9228d9949b546f7fed1a682531029d0
ChaRedmiBook <varus552@gmail.com>
2022-07-30 01:21:43 +0700

    Ex 10.7 Account Manager Completed

--- src/com/khoalt/IntroductionEbook/Chap10/Ex107/AccountManager.java
+++ src/com/khoalt/IntroductionEbook/Chap10/Ex107/AccountManager.java
@@ -5,8 +5,7 @@ public class AccountManager {
 
     public Account[] createAccounts() {
         for (int i = 0; i < 10; i++) {
-            accountList[i].setId(i);
-            accountList[i].setBalance(100);
+            this.accountList[i] = new Account(i, 100, 0.1);
         }
         return accountList;
     }
@@ -14,4 +13,29 @@ public class AccountManager {
     public Account selectAccount(int id) {
         return accountList[id];
     }
+
+    public void showBalance(int id) {
+        System.out.println("Account " + id + " current balance: " + accountList[id].getBalance());
+    }
+
+    public boolean withdrawAmount(int id, double amount) {
+        if (amount > accountList[id].getBalance()) {
+            System.out.println("Not enough money");
+            return false;
+        } else {
+            accountList[id].setBalance(accountList[id].getBalance() - amount);
+            System.out.println("Succes");
+            return true;
+        }
+    }
+
+    public boolean depositAmount(int id, double amount) {
+        accountList[id].setBalance(accountList[id].getBalance() + amount);
+        System.out.println("Success");
+        return true;
+    }
+
+
+
+
 }
--- src/com/khoalt/IntroductionEbook/Chap10/Ex107/Main.java
+++ src/com/khoalt/IntroductionEbook/Chap10/Ex107/Main.java
@@ -5,26 +5,56 @@ import java.util.Scanner;
 public class Main {
     static Scanner input = new Scanner(System.in);
     static AccountManager am = new AccountManager();
+    static int id;
 
     public static void main(String[] args) {
-        am.createAccounts();
-        System.out.println("Nhap chinh xac id: ");
-        int id = input.nextInt();
-        if (id < 10) {
-            Account selectedAccount = am.selectAccount(id);
-        } else {
-            System.out.println("Error");
-        }
+        am.createAccounts(); //Tao 10 account
+        // Loc ID
+        do {
+            System.out.print("Your ID: ");
+            id = input.nextInt();
+        } while (id >= 10);
+        Account selectedAccount = am.selectAccount(id);
+        // Tao menu
+        int choice;
+        do {
+            System.out.println("1. View the current balance");
+            System.out.println("2. Withdraw money");
+            System.out.println("3. Deposit money");
+            System.out.println("4. Exit");
+            choice = input.nextInt();
+            input.nextLine();
+            switch (choice) {
+                case 1: {
+                    viewBalance();
+                    break;
+                }
+                case 2: {
+                    withdrawMoney();
+                    break;
+                }
+                case 3: {
+                    depositMoney();
+                    break;
+                }
+            }
 
-//        int choice;
-//        do {
-//            System.out.println("1. View the current balance");
-//            System.out.println("2. Withdraw money");
-//            System.out.println("3. Deposit money");
-//            System.out.println("4. Exit");
-//            choice = input.nextInt();
-//            input.nextLine();
-//
-//        } while (choice != 0);
+        } while (choice != 4);
+    }
+
+    public static void viewBalance() {
+        am.showBalance(id);
+    }
+
+    public static void withdrawMoney() {
+        System.out.print("Withdraw amount: ");
+        double amount = input.nextDouble();
+        am.withdrawAmount(id, amount);
+    }
+
+    public static void depositMoney() {
+        System.out.print("Deposit amount: ");
+        double amount = input.nextDouble();
+        am.depositAmount(id, amount);
     }
 }
