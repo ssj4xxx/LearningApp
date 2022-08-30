0b24e7bf004f7f3dd11cd91678d85236507b95b6
SSJ4 <varus552@gmail.com>
2022-08-30 13:45:33 +0700

    Chapter 12 6

---
+++ src/com/khoalt/IntroductionEbook/Chap12/Exercise12_11.java
@@ -0,0 +1,38 @@
+package com.khoalt.IntroductionEbook.Chap12;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Exercise12_11 {
+    public static void main(String[] args) throws Exception {
+        // Check command line parameter usage
+        if (args.length != 2) {
+            System.out.println(
+                    "Usage: java ReplaceText sourceFile targetFile oldStr newStr");
+            System.exit(1);
+        }
+
+        // Check if file exists
+        File textFile = new File(args[1]);
+        if (!textFile.exists()) {
+            System.out.println("Text file " + args[1] + " does not exist");
+            System.exit(2);
+        }
+
+        try (
+                // Create input and output files
+                Scanner input = new Scanner(textFile);
+                PrintWriter output = new PrintWriter(textFile);
+        ) {
+            int count = 0;
+            while (input.hasNext()) {
+                String s1 = input.nextLine();
+                if (s1.equals(args[0])) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
--- src/com/khoalt/Test1.java
+++ src/com/khoalt/Test1.java
@@ -1,7 +1,5 @@
 package com.khoalt;
 
-import java.util.Scanner;
-
 public class Test1 {
 
     public static int quotient(int number1, int number2) {
@@ -11,21 +9,9 @@ public class Test1 {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // Prompt the user to enter two integers
-        System.out.print("Enter two integers: ");
-        int number1 = input.nextInt();
-        int number2 = input.nextInt();
-        try {
-            int result = quotient(number1, number2);
-            System.out.println(number1 + " / " + number2 + " is "
-                    + result);
-        }
-        catch (ArithmeticException ex) {
-            System.out.println("Exception: an integer " +
-                    "cannot be divided by zero ");
-        }
-        System.out.println("Execution continues ...");
+        Integer number = 1;
+        boolean isInt = number instanceof Integer;
+        System.out.println(number.getClass().getName() + isInt);
     }
 
 }
