915b9f8f745a76ba031543e4359cc3782fd10017
SSJ4 <varus552@gmail.com>
2022-09-21 18:46:11 +0700

    Chap 17 Ex

---
+++ src/com/khoalt/IntroductionEbook/Chap17/Ex1704/Ex1704write.java
@@ -0,0 +1,17 @@
+package com.khoalt.IntroductionEbook.Chap17.Ex1704;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Ex1704write {
+    public static void main(String[] args) throws IOException {
+        try (
+                DataOutputStream output = new DataOutputStream(new FileOutputStream("Ex1704.dat"));
+        ) {
+            output.writeInt(001);
+            output.writeUTF("Khoa");
+            output.writeDouble(95.99);
+        }
+    }
+}
