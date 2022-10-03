d737a3a4c93ff9fda9c0196051d6ee937bc68c67
SSJ4 <varus552@gmail.com>
2022-10-03 23:19:53 +0700

    Chapter 20 thay Dang

--- src/com/khoalt/IntroductionEbook/Chap19/ExThayDangRemoveDuplicates.java
+++ src/com/khoalt/IntroductionEbook/Chap19/ExThayDangRemoveDuplicates.java
@@ -1,9 +1,13 @@
 package com.khoalt.IntroductionEbook.Chap19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class ExThayDangRemoveDuplicates {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Integer> intList = new ArrayList<>();
         intList.add(1);
         intList.add(2);
@@ -11,20 +15,32 @@ public class ExThayDangRemoveDuplicates {
         intList.add(1);
         intList.add(2);
         intList.add(5);
-        System.out.println("Old list: " + intList.toString());
-        intList = removeDuplicates(intList);
-        System.out.println("New list: " + intList.toString());
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(3);
+        stack.push(5);
+        stack.push(1);
+        stack.push(1);
+        stack.push(7);
+        stack.push(3);
+//        System.out.println("Old list: " + stack.toString());
+//        removeDuplicates(stack);
+//        System.out.println("New list: " + stack.toString());
+        TextFileCollection file = new TextFileCollection(new File("testrd.txt"));
+        System.out.println(file);
+        removeDuplicates(file);
+        System.out.println(file);
     }
 
-    public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
-        ArrayList<E> finalList = new ArrayList<E>();
-        int index = 0;
-        while (index < list.size()) {
-            if (!finalList.contains(list.get(index))) {
-                finalList.add(list.get(index));
+    //TODO: implement
+    public static <E> void removeDuplicates(List<E> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) == list.get(j)) {
+                    list.remove(j);
+                    j--;
+                }
             }
-            index++;
         }
-        return finalList;
     }
 }
