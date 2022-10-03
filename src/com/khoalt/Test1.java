ce223cf84391e5b0350796e6bdba7a7bd7d8a634
SSJ4 <varus552@gmail.com>
2022-10-03 15:35:58 +0700

    Chapter 19 Finish

--- src/com/khoalt/Test1.java
+++ src/com/khoalt/Test1.java
@@ -2,13 +2,12 @@ package com.khoalt;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Test1 {
     public static void main(String[] args) throws IOException {
-        ArrayList<Date> dates =
-                new ArrayList<>();
-        dates.add(new Date());
-        Date date = dates.get(0);
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        System.out.println(list1 instanceof ArrayList);
+        System.out.println(list2 instanceof ArrayList);
     }
 }
