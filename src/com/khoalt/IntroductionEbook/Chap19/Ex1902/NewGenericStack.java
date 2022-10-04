39d8be045d4e135fd1274fa5682ff934a141d5f4
SSJ4 <varus552@gmail.com>
2022-10-04 22:30:55 +0700

    Chapter 20 thay Dang

---
+++ src/com/khoalt/IntroductionEbook/Chap19/Ex1902/NewGenericStack.java
@@ -0,0 +1,25 @@
+package com.khoalt.IntroductionEbook.Chap19.Ex1902;
+
+import java.util.ArrayList;
+
+public class NewGenericStack<E> extends ArrayList<E> {
+    public E peek() {
+        return get(size() - 1);
+    }
+
+    public void push(E o) {
+        add(o);
+    }
+
+
+    public E pop() {
+        E o = get(size() - 1);
+        remove(size() - 1);
+        return o;
+    }
+
+    @Override
+    public String toString() {
+        return "stack: " + super.toString();
+    }
+}
---
+++ src/com/khoalt/IntroductionEbook/Chap19/Ex1903/Pair.java
@@ -0,0 +1,13 @@
+package com.khoalt.IntroductionEbook.Chap19.Ex1903;
+
+import com.khoalt.Circle;
+
+public class Pair {
+    private Circle c;
+
+    public Pair() {};
+
+    public Pair(double radius) {
+        c.setRadius(radius);
+    }
+}
