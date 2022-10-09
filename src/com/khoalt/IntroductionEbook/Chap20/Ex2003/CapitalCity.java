3b1db7d64f999ca80d9fca87b66e12dc88975b8e
SSJ4 <varus552@gmail.com>
2022-10-09 18:27:20 +0700

    Chapter 19 20

---
+++ src/com/khoalt/IntroductionEbook/Chap20/Ex2003/CapitalCity.java
@@ -0,0 +1,23 @@
+package com.khoalt.IntroductionEbook.Chap20.Ex2003;
+
+public class CapitalCity {
+    private String state;
+    private String city;
+
+    public CapitalCity(){}
+
+    public CapitalCity(String state, String city) {
+        this.state = state;
+        this.city = city;
+    }
+
+    public String getCity() {return city;}
+    public String getState() {return state;}
+    public void setCity(String city) {this.city = city;}
+    public void setState(String state) {this.state = state;}
+
+    @Override
+    public String toString() {
+        return state + " " + city;
+    }
+}
