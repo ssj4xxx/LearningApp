97f5769fc1e6aeda6818bcedab1044846c38df0b
SSJ4 <varus552@gmail.com>
2022-10-22 11:24:57 +0700

    Chapter 33

--- src/com/khoalt/TestKhoa/Cht33/testpackage/Client.java
+++ src/com/khoalt/TestKhoa/Cht33/testpackage/Client.java
@@ -2,24 +2,23 @@ package com.khoalt.TestKhoa.Cht33.testpackage;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Client {
     public static void main(String[] args) {
         int port = 8000;
 
+        String host = "localhost"
         DataInputStream in;
         DataOutputStream out;
-        ServerSocket server;
         Socket socket;
 
-//        server = new ServerSocket(port);
-//        socket = server.accept();
+//        socket = new Socket(host, port);
 //        in = new DataInputStream(socket.getInputStream());
 //        out = new DataOutputStream(socket.getOutputStream());
+//
 //        System.out.println(in.readDouble());
+//
 //        out.writeDouble(aNumber);
     }
-
 }
