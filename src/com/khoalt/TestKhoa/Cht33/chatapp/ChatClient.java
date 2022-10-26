7a466200eb6a516ff8f13addc63ed11eeff61825
SSJ4 <varus552@gmail.com>
2022-10-26 11:02:29 +0700

    Chapter 33 Chat App

--- src/com/khoalt/TestKhoa/Cht33/chatapp/ChatClient.java
+++ src/com/khoalt/TestKhoa/Cht33/chatapp/ChatClient.java
@@ -1,33 +1,69 @@
 package com.khoalt.TestKhoa.Cht33.chatapp;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ChatClient {
-    private DataOutputStream toServer;
-    private DataInputStream fromServer;
-    private Scanner input = new Scanner(System.in);
+    private Socket socket;
+    private BufferedReader inputFromServer;
+    private BufferedWriter outputToServer;
+    private String clientName;
     public static void main(String[] args) {
         new ChatClient();
     }
     public ChatClient() {
         try {
-            Socket socket = new Socket("localhost", 1234);
-            toServer = new DataOutputStream(socket.getOutputStream());
-            fromServer = new DataInputStream(socket.getInputStream());
-            String name, message;
-            while (true) {
-                System.out.println("Enter your name: ");
-                name = input.nextLine();
-                toServer.writeUTF(name);
-                while (true) {
-                    message = input.nextLine();
-                    toServer.writeUTF(message);
+            socket = new Socket("localhost", 1234);
+            inputFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outputToServer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            System.out.print("Enter your name: ");
+            clientName = inputFromServer.readLine();
+            outputToServer.write(clientName);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void sendMessage() {
+        try {
+//            outputToServer.write();
+            Scanner inputFromKeyboard = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageToSend = inputFromKeyboard.nextLine();
+                outputToServer.write(messageToSend);
+                outputToServer.newLine();
+                outputToServer.flush();
+            }
+        } catch (IOException ex) {
+            closeSocket(socket, inputFromServer, outputToServer);
+        }
+    }
+    public void receiveMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String messageFromServer;
+                while (socket.isConnected()) {
+                    try {
+                        messageFromServer = inputFromServer.readLine();
+                        System.out.println(messageFromServer);
+                    } catch (IOException ex) {
+                        closeSocket(socket, inputFromServer, outputToServer);
+                    }
                 }
-
+            }
+        }).start();
+    }
+    public void closeSocket(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
             }
         } catch (IOException ex) {
             ex.printStackTrace();
--- src/com/khoalt/TestKhoa/Cht33/chatapp/ChatServer.java
+++ src/com/khoalt/TestKhoa/Cht33/chatapp/ChatServer.java
@@ -1,52 +1,108 @@
 package com.khoalt.TestKhoa.Cht33.chatapp;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ChatServer {
-    public static void main(String[] args) {
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    public static void main(String[] args) throws IOException {
         new ChatServer();
     }
     public ChatServer() {
         try {
-            ServerSocket serverSocket = new ServerSocket(1234);
+            serverSocket = new ServerSocket(1234);
             System.out.println("Conversation started at " + new Date());
-            while (true) {
-                Socket socket = serverSocket.accept();
-                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-                InetAddress inetAddress = socket.getInetAddress();
-                String clientName = inputFromClient.readUTF();
-                System.out.println("Client " + clientName + " has joined the conversation");
-                ClientHandler task = new ClientHandler(socket);
-                new Thread(task).start();
+            while (!serverSocket.isClosed()) {
+                socket = serverSocket.accept();
+                System.out.println("A new client has connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                new Thread(clientHandler).start();
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
+    public void closeChatServer() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     class ClientHandler implements Runnable {
+        public static ArrayList<ClientHandler> clients = new ArrayList<>();
         private Socket socket;
+        private BufferedReader inputFromClient;
+        private BufferedWriter outputToClient;
+        private String clientName;
         public ClientHandler(Socket socket) {
-            this.socket = socket;
+            try {
+                this.socket = socket;
+                this.outputToClient = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                this.inputFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.clientName = inputFromClient.readLine();
+                clients.add(this);
+                InetAddress inetAddress = socket.getInetAddress();
+                System.out.println("Client " + clientName + " at " + inetAddress + " has joined the conversation");
+            } catch (IOException ex) {
+
+            }
+
+
         }
         @Override
         public void run() {
-            try {
-                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-                while (true) {
-                    String message = inputFromClient.readUTF();
-                    outputToClient.writeDouble(area);
+            String messageFromClient;
+            while (socket.isConnected()) {
+                try {
+                    messageFromClient = inputFromClient.readLine();
+                    broadcastMessage(messageFromClient);
+                } catch (IOException ex) {
+                    closeSocket(socket, inputFromClient, outputToClient);
+                    break;
+                }
+            }
+        }
 
-                    System.out.println("radius received from the client: " + radius + '\n');
-                    System.out.println("Area found: " + area + '\n');
+        public void broadcastMessage(String message) {
+            for (ClientHandler clientHandler : clients) {
+                try {
+                    if (!clientHandler.clientName.equals(clientName)) {
+                        clientHandler.outputToClient.write(message);
+                        clientHandler.outputToClient.newLine();
+                        clientHandler.outputToClient.flush();
+                    }
+                } catch (IOException ex) {
+                    closeSocket(socket, inputFromClient, outputToClient);
+                }
+            }
+        }
+
+        public void removeCliendHandler() {
+            clients.remove(this);
+            broadcastMessage(clientName + " has left the conversation.");
+        }
+
+        public void closeSocket(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+            removeCliendHandler();
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+                if (socket != null) {
+                    socket.close();
                 }
             } catch (IOException ex) {
                 ex.printStackTrace();
